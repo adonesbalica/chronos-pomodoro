cd6f1c865764b9e151ccf97cb48600040627911c
Adones Balica <adonesbalica@gmail.com>
2025-12-11 09:53:35 -0300

    feat: updating the counter with webworker

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,6 +2,7 @@ import { useEffect, useReducer } from 'react';
 import { TaskContext } from './TaskContext';
 import { initialTaskState } from './initialStateTask';
 import { taskReducer } from './taskReducer';
+import { TimerWorkManager } from '../../workers/TimerWorkManager';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -10,7 +11,26 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
-  useEffect(() => {}, []);
+  const worker = TimerWorkManager.getIstance();
+
+  worker.onmessage(e => {
+    const countDownSeconds = e.data;
+    console.log(countDownSeconds);
+
+    if (countDownSeconds <= 0) {
+      console.log('Worker terminado por falta de activeTask');
+      worker.terminate();
+    }
+  });
+
+  useEffect(() => {
+    if (!state.activeTask) {
+      console.log('Worker terminado por falta de activeTask');
+      worker.terminate();
+    }
+
+    worker.postMessage(state);
+  }, [worker, state]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
